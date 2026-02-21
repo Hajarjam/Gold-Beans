60318d951aeebf17be01b739b414fcf50ad4db86
hajarjam <Hajarjamal774@gmail.com>
2026-02-21 00:43:17 +0000

    client routes update

--- frontend/src/router/ClientRoutes.jsx
+++ frontend/src/router/ClientRoutes.jsx
@@ -5,6 +5,9 @@ import ProtectedRoute from "./ProtectedRoutes";
 
 import HomePage from "../pages/public/HomePage";
 import ProductCart from "../pages/public/ProductCart";
+import PayementPage from "../pages/public/PayementPage";
+import OrderConfirmed from "../pages/public/OrderConfirmed";
+
 import SubHistoryPage from "../pages/client/SubHistoryPage";
 import ProfilePage from "../pages/client/ProfilePage";
 import CoffeesPage from "../pages/public/CoffeesPage";
@@ -12,8 +15,6 @@ import ClientDashboard from "../pages/client/clientDashboardPage";
 
 const ClientRoutes = () => {
   const { user, role } = useAuth();
-import PayementPage from "../pages/public/PayementPage";
-import OrderConfirmed from "../pages/public/OrderConfirmed";
 
   // redirect if not logged in
   if (!user) return <Navigate to="/login" replace />;
@@ -36,11 +37,10 @@ import OrderConfirmed from "../pages/public/OrderConfirmed";
         <Route path="cart" element={<ProductCart />} />
         <Route path="payment" element={<PayementPage />} />
         <Route path="confirmed" element={<OrderConfirmed />} />
-        <Route path="*" element={<Navigate to="/client" replace />} />
       </Route>
 
       {/* fallback */}
-      <Route path="*" element={<Navigate to="." replace />} />
+      <Route path="*" element={<Navigate to="/client" replace />} />
     </Routes>
   );
 };
